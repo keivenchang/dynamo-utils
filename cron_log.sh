ee941825a3195b651033aee20ec24670d81c98e8
Keiven Chang <keivenchang@users.noreply.github.com>
2026-01-06 22:33:23 -0800

    cron: re-enable wrapper by removing marker-file disable

--- cron_log.sh
+++ cron_log.sh
@@ -20,11 +20,10 @@ fi
 JOB_NAME="$1"
 shift
 
-# Testing / maintenance kill-switch:
+# Optional maintenance kill-switch:
 # - Set `DYNAMO_UTILS_DISABLE_CRON=1` to skip ALL cron-invoked jobs that use this wrapper.
-# - Or create `~/.cache/dynamo-utils/disable-cron` (no need to edit system crontab).
-if [ "${DYNAMO_UTILS_DISABLE_CRON:-}" = "1" ] || [ -f "${HOME}/.cache/dynamo-utils/disable-cron" ]; then
-  echo "cron_log.sh: disabled (DYNAMO_UTILS_DISABLE_CRON=1 or ~/.cache/dynamo-utils/disable-cron exists); skipping job=${JOB_NAME}" >&2
+if [ "${DYNAMO_UTILS_DISABLE_CRON:-}" = "1" ]; then
+  echo "cron_log.sh: disabled (DYNAMO_UTILS_DISABLE_CRON=1); skipping job=${JOB_NAME}" >&2
   exit 0
 fi
 
